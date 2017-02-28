9bc09f76529d3f01c204694c4c4162bcce1ec353
Trevor Dodds <tds@esi-group.com>
2017-02-28 17:30:55 +0100

    Separate debug/release mode

--- compile.sh
+++ compile.sh
@@ -6,11 +6,11 @@ mkdir install-release
 mkdir build
 cd build
 
-# No difference between debug/release here
-cmake -DCMAKE_INSTALL_PREFIX=../install-release -G Ninja ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install-release -G Ninja ..
 ninja
 ninja install
 
-cmake -DCMAKE_INSTALL_PREFIX=../install-debug -G Ninja ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install-debug -G Ninja ..
 ninja
 ninja install
+
